a48521e918b0cf48a30407f260d108268179d802
Scott Zeta <scott-zeta@outlook.com>
2025-03-10 15:25:59 +1030

    enhance SplashScreen with animated icon and updated styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,15 @@
-import { View, Text } from 'react-native';
+import { View, Text, Animated } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function SplashScreen() {
   return (
-    <View>
-      <Text className="text-3xl color-blue-400">SpalshScreen</Text>
+    <View className="flex-1 justify-center items-center bg-green-500">
+      <Animated.View>
+        <Ionicons name="medical" size={100} color="white" />
+      </Animated.View>
+      <Text className="text-white text-4xl font-bold mt-5 tracking-wider">
+        Pills Reminder
+      </Text>
     </View>
   );
 }
