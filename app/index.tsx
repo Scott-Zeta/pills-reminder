5150f04c5e4177e3f50fd7e70989fa7e7eff82d0
Scott Zeta <scott-zeta@outlook.com>
2025-03-10 15:04:55 +1030

    add initial setup for NativeWind and Tailwind CSS integration

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function SplashScreen() {
+  return (
+    <View>
+      <Text className="text-3xl color-blue-400">SpalshScreen</Text>
+    </View>
+  );
+}
