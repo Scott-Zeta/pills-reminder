ecee748e29235be65154b32206a4e58d6bd4c516
Scott Zeta <scott-zeta@outlook.com>
2025-03-16 19:51:46 +1030

    add skeleton for medication details in AddMedicationScreen

--- app/(page)/medications/add.tsx
+++ app/(page)/medications/add.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native';
 import PageHeader from '@/components/ui/PageHeader';
 
 export default function AddMedicationScreen() {
@@ -6,9 +6,59 @@ export default function AddMedicationScreen() {
     <>
       <PageHeader title="Add Medication" />
       <View className="flex-1 bg-gray-100">
-        <ScrollView>
-          <Text>AddMedicationScreen</Text>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          {/* Basic Info */}
+          <View className="mb-6">
+            <Text className="text-2xl font-bold text-gray-900 mb-4 mt-2">
+              Name
+            </Text>
+          </View>
+          <View className="mb-6">
+            <Text className="text-2xl font-bold text-gray-900 mb-4 mt-2">
+              Dosage
+            </Text>
+          </View>
+
+          {/* Date */}
+          <View className="mb-6">
+            <Text className="text-2xl font-bold text-gray-900 mb-4 mt-2">
+              When will you take this?
+            </Text>
+          </View>
+
+          {/* Time */}
+          <View className="mb-6">
+            <Text className="text-2xl font-bold text-gray-900 mb-4 mt-2">
+              At what time?
+            </Text>
+          </View>
+
+          {/* Duration */}
+          <View className="mb-6">
+            <Text className="text-2xl font-bold text-gray-900 mb-4 mt-2">
+              Duration
+            </Text>
+          </View>
+
+          {/* Reminder toggle */}
+          <View className="mb-6">
+            <Text>Reminder toggle</Text>
+          </View>
+
+          {/* Refill Toggle */}
+          <View className="mb-6">
+            <Text>Refill Toggle</Text>
+          </View>
         </ScrollView>
+        {/* Button */}
+        <View>
+          <TouchableOpacity>
+            <Text>Add Medication</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>Cancel</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </>
   );
