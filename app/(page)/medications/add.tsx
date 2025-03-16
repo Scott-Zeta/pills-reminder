f118cf67f2f0b862518d02de11448878315a09c9
Scott Zeta <scott-zeta@outlook.com>
2025-03-16 20:16:57 +1030

    enhance AddMedicationScreen with input fields for medication name, dosage, and reminders

--- app/(page)/medications/add.tsx
+++ app/(page)/medications/add.tsx
@@ -1,4 +1,10 @@
-import { View, Text, ScrollView, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
 import PageHeader from '@/components/ui/PageHeader';
 
 export default function AddMedicationScreen() {
@@ -6,47 +12,64 @@ export default function AddMedicationScreen() {
     <>
       <PageHeader title="Add Medication" />
       <View className="flex-1 bg-gray-100">
-        <ScrollView showsVerticalScrollIndicator={false}>
+        <ScrollView showsVerticalScrollIndicator={false} className="p-5">
           {/* Basic Info */}
-          <View className="mb-6">
-            <Text className="text-2xl font-bold text-gray-900 mb-4 mt-2">
-              Name
-            </Text>
+          <View className="mb-1">
+            <Text className="text-xl font-bold text-gray-900 my-2">Name</Text>
+            <View className="bg-white rounded-xl mb-3 border border-gray-300 shadow-sm">
+              <TextInput
+                className="text-xl text-gray-800 mx-4 my-1 h-10"
+                placeholder="Medication Name"
+                placeholderTextColor="#999"
+                value=""
+                onChangeText={() => {}}
+              />
+            </View>
           </View>
-          <View className="mb-6">
-            <Text className="text-2xl font-bold text-gray-900 mb-4 mt-2">
+
+          <View className="mb-1">
+            <Text className="text-2xl font-bold text-gray-900 my-2">
               Dosage
             </Text>
+            <View className="bg-white rounded-xl mb-3 border border-gray-300 shadow-sm">
+              <TextInput
+                className="text-xl text-gray-800 mx-4 my-1 h-10"
+                placeholder="Medication Name"
+                placeholderTextColor="#999"
+                value=""
+                onChangeText={() => {}}
+              />
+            </View>
           </View>
 
           {/* Date */}
-          <View className="mb-6">
-            <Text className="text-2xl font-bold text-gray-900 mb-4 mt-2">
+          <View className="mb-1">
+            <Text className="text-2xl font-bold text-gray-900 my-2">
               When will you take this?
             </Text>
           </View>
 
           {/* Time */}
-          <View className="mb-6">
-            <Text className="text-2xl font-bold text-gray-900 mb-4 mt-2">
+          <View className="mb-1">
+            <Text className="text-2xl font-bold text-gray-900 my-2">
               At what time?
             </Text>
           </View>
 
           {/* Duration */}
-          <View className="mb-6">
-            <Text className="text-2xl font-bold text-gray-900 mb-4 mt-2">
+          <View className="mb-1">
+            <Text className="text-2xl font-bold text-gray-900 my-2">
               Duration
             </Text>
           </View>
 
           {/* Reminder toggle */}
-          <View className="mb-6">
+          <View className="mb-1">
             <Text>Reminder toggle</Text>
           </View>
 
           {/* Refill Toggle */}
-          <View className="mb-6">
+          <View className="mb-1">
             <Text>Refill Toggle</Text>
           </View>
         </ScrollView>
